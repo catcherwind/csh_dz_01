3db03ef8ab2a4537809f993d198374ed971fee4e
Ksusha <ksushalive@yandex.ru>
2022-11-21 21:05:22 +0300

    init: solution of task 2

---
+++ z02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = int.Parse(Console.ReadLine());
+if ((a >= b) && (a >= c))
+{
+    Console.WriteLine($"Больше первое число - {a}");
+}
+else if ((b >= a) && (b >= c))
+{
+    Console.WriteLine($"Больше второе число - {b}");
+}
+else 
+{
+    Console.WriteLine($"Больше третье число - {c}");
+}
