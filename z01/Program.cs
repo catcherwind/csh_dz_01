4ea4cf090fd10c87caeac8d3212a08ced3a8e3c4
Ksusha <ksushalive@yandex.ru>
2022-11-21 21:00:11 +0300

    init: solution of task 1

---
+++ z01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"Больше первое число - {a}");
+}
+else{
+    Console.WriteLine($"Больше второе число - {b}");
+}
